0868e86505b3a123938686a3c9f5ad6c4f24702c
Elizabeth Phillips <elizabeth_phillips@homedepot.com>
2019-06-12 11:47:16 -0500

    Added function exercises

---
+++ function_practice.py
@@ -0,0 +1,23 @@
+"""
+title: function_practice
+author: Elizabeth
+date: 2019-06-12 11:21
+"""
+
+
+def age_calculator(current, birth):
+    return current - birth
+
+
+print(age_calculator(2019, 1924))
+print(age_calculator(2019, 1832))
+print(age_calculator(2019, 2005))
+
+
+def avg_numbers(x, y, z):
+    return (x + y + z) / 3
+
+
+print(avg_numbers(10, 24, 65))
+print(avg_numbers(3, 4, 5))
+print(avg_numbers(2, 80, 90))
--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,41 @@ author: Elizabeth
 date: 2019-06-11 10:52
 """
 
-import random
+# import random
+#
+# print(random.choice("hello"))
 
-print(random.choice("hello"))
+
+import random
+import math
+
+
+def dist_form(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+x1 = int(input("x1: "))
+x2 = int(input("x2: "))
+y1 = int(input("y1: "))
+y2 = int(input("y2: "))
+
+output = dist_form(x1, y1, x2, y2)
+print(output)
+#
+# def get_raise(name, salary):
+#     print(f"My name is {name} and salary is {salary}")
+#
+#     # rest of work
+#     raise_per = random.randint(0, 100)
+#     raise_amount = salary * raise_per / 100 + salary
+#     print(f"Your raiser is {raise_per}% of ${salary}")
+#     print(f"{name}, your new salary is ${raise_amount}")
+#
+#
+# get_raise("bob", 12345)
+# print("="*30)
+# get_raise("elizabeth", 543)
+# print("="*30)
+# get_raise("sue", 23)
+# print("="*30)
+# get_raise("joe", 86)
