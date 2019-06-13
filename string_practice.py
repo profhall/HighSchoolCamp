c80146e4c45a98f45f80fa8f27159bb876f1cfeb
Elizabeth Phillips <elizabeth_phillips@homedepot.com>
2019-06-13 11:03:49 -0500

    Added string practice labs

--- string_practice.py
+++ string_practice.py
@@ -4,22 +4,43 @@ author: Elizabeth
 date: 2019-06-11 13:45
 """
 
-favorite_things = """Some of my favorite things are bird watching, jogging,
-listening to podcasts, and coding in Python."""
+phrase = "here i am!"
+slogan = "jskldfjksd"
+combined = f"{phrase} {slogan}\n"
+print(combined)
 
-x = favorite_things[3:15:2]
 
-print(x)
+def is_letter(character):
+    return character[0].lower() in "qwertyuiopasdfghjklzxcvbnm"
 
-my_name = "Emily"
-print("E" in my_name or "e" in my_name)
-print(len(my_name))
 
-x = my_name.upper()
-print(type(x))
-print(my_name)
+print(is_letter("isadfasdf"))
+print(is_letter("0"))
 
-greetings = input("Enter greeting here")
-new_greeting = greetings.replace("salve", "")
-print(new_greeting)
 
+def short_hand(short):
+    short = short.lower().replace("and", "&").replace("too", "2").replace("you", "U").replace("for", "4")
+    short = short.replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "")
+    return short
+
+
+print(short_hand("How are you today?"))
+
+
+def removing(check):
+    check = check.lower().replace(" ", "").replace("'", "").replace(",", "").replace("!", "").replace(".", "")\
+        .replace(";", "")
+    return check
+
+
+clear = removing("Madam, I'm Adam")
+print(clear)
+
+
+def palindrome(check):
+    check = removing(check)
+    return check == check[::-1]
+
+
+print(palindrome("Madam, I'm Adam"))
+print(palindrome("Computer"))
